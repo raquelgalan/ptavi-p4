ae5a8f702ee42549ed106c5436662a5f6e852294
raquel galan montes <rgalan@iota19.aulas.gsyc.es>
2014-10-20 10:55:13 +0200

    server.py ejercicio5 imprime IP y puerto del cliente

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ en UDP simple
 """
 
 import SocketServer
+import sys
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -15,6 +16,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
+        print self.client_address
         self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -25,6 +27,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
+    PORT = int(sys.argv[1])
+    serv = SocketServer.UDPServer(("", PORT), EchoHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
