3316ca4ddb007650bee4130e2c548bd6f5f2a9d0
raquel galan montes <rgalan@iota09.aulas.gsyc.es>
2014-10-27 13:12:04 +0100

    ejercicio 6: el servidor guarda la direccion y la IP en un diccionario

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
+#Raquel Galan Montes
 """
 Clase (y programa principal) para un servidor de eco
 en UDP simple
@@ -8,8 +9,8 @@ en UDP simple
 import SocketServer
 import sys
 
-
-class EchoHandler(SocketServer.DatagramRequestHandler):
+diccionario_clientes = {}
+class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
@@ -17,17 +18,29 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         print self.client_address
-        self.wfile.write("Hemos recibido tu peticion")
+        self.wfile.write("Hemos recibido tu peticion ")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print "El cliente nos manda " + line
-            if not line:
+            text = self.rfile.read()
+            if text != "":
+                print "El cliente nos manda " + text
+                mensaje = text.split()
+                metodo = mensaje[0]
+                email = mensaje[1]
+                if metodo == "REGISTER":
+                    self.wfile.write ("SIP/1.0 200 OK\r\n\r\n")
+                    diccionario_clientes[email] = self.client_address
+                    print diccionario_clientes
+            else:
                 break
+            
+
+
+        
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     PORT = int(sys.argv[1])
-    serv = SocketServer.UDPServer(("", PORT), EchoHandler)
+    serv = SocketServer.UDPServer(("", PORT), SIPRegisterHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
